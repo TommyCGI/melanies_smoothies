61532105b054c163ed10dc3c392a82b68c98f996
TommyCGI <tommy.smits@cgi.com>
2024-10-08 13:31:43 +0200

    Update Steamlit_app.py

--- Steamlit_app.py
+++ Steamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+streamlit.title('my parents new healthy diner')
 # Write directly to the app
 st.title("Customize Your Smoothie!")
 st.write(
